3a443048f94aaa9134cd0af36a6001daa21f687b
RacmanT <tibor.racman@gmail.com>
2020-12-13 17:46:21 +0100

    added testIllegalSymbolException in MoveTester

--- src/test/java/it/units/sdm/project/MoveTester.java
+++ src/test/java/it/units/sdm/project/MoveTester.java
@@ -2,14 +2,13 @@ package it.units.sdm.project;
 
 import it.units.sdm.project.entities.Move;
 import it.units.sdm.project.exceptions.BoardIndexOutOfBoundsException;
+import it.units.sdm.project.exceptions.IllegalSymbolException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MoveTester {
-
-
 
     @ParameterizedTest
     @ValueSource(strings = {"0 3 circle", "7 3 cross", "-1 1 cross", "102 2 cross",    //row exception case
@@ -18,6 +17,15 @@ public class MoveTester {
     void testBoardIndexOutOfBoundsException(String command) {
         BoardIndexOutOfBoundsException thrown = assertThrows(
                 BoardIndexOutOfBoundsException.class,
+                () -> new Move(command));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 3 kross", "2 3 cyrcle", "1 5 circl", "1 2 crss", "3 2 cros"})
+    void testIllegalSymbolException(String command) {
+        IllegalSymbolException thrown = assertThrows(
+                IllegalSymbolException.class,
                 () -> new Move(command));
     }
 
