95dbd83106252a5d1d2f80662273ed8fb035d0a8
PalMassimo <massimo.palmisano02@gmail.com>
2020-12-20 12:03:19 +0100

    delete gui package using Swing library and modify build.gradle script to accomate JavaFX library

--- src/main/java/it/units/sdm/project/Play.java
+++ src/main/java/it/units/sdm/project/Play.java
@@ -1,15 +1,12 @@
 package it.units.sdm.project;
 
-import it.units.sdm.project.gui.GUIGame;
+import it.units.sdm.project.console.ConsoleGame;
 
 public class Play {
 
     public static void main(String[] args) {
 
-//        ConsoleGame game = new ConsoleGame();
-//        game.start();
-
-        GUIGame game = new GUIGame();
+        ConsoleGame game = new ConsoleGame();
         game.start();
 
     }
