c9aab2c11427fb9723ab520cc126c7199c5cbe77
PalMassimo <massimo.palmisano02@gmail.com>
2020-12-30 20:23:11 +0100

    Now hyperlink is responsive and lead to project repository

--- src/main/java/sdm/project/guicomponents/controllers/AboutController.java
+++ src/main/java/sdm/project/guicomponents/controllers/AboutController.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,12 +18,17 @@ public class AboutController implements Initializable {
     @FXML
     TextFlow credits;
 
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Label label = new Label("Our Project can be found on Github:");
-        Hyperlink hyperlink = new Hyperlink("https://github.com/RacmanT/ChaosAndOrder");
+        Hyperlink hyperlink = new Hyperlink("http://github.com/RacmanT/ChaosAndOrder");
+        hyperlink.setOnAction(event-> {
+            try {
+                Desktop.getDesktop().browse(new URI(hyperlink.getText()));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
         credits.getChildren().addAll(label, hyperlink);
 
     }
